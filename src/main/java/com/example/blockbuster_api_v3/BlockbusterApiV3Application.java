aa7e9cc39fcd0ff2d68d526248a3cbff72ec1729
Shanilka Haturusinghe <s.haturusinghe99@gmail.com>
2025-05-14 12:37:33 +0530

    Initial commit

---
+++ src/main/java/com/example/blockbuster_api_v3/BlockbusterApiV3Application.java
@@ -0,0 +1,13 @@
+package com.example.blockbuster_api_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockbusterApiV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockbusterApiV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blockbuster_api_v3/BlockbusterApiV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockbuster_api_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockbusterApiV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
